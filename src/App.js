d804a1799850031400affdd2b771142e395bf235
Subhash <polisettipvvv@gmail.com>
2022-01-07 16:47:40 +0530

    created login and signup page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./components/signup";
+import Login from "./components/login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<SignUp />} />
+				<Route path="/login" element={<Login />} />
+				{/* <Route path="/" element={<Issues />} />
+		<Route path="/issues/:id" element={<SingleIssue />} /> */}
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/components/login.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { Card, Form, Button } from "react-bootstrap";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+	const navigate = useNavigate();
+	const [formValidated, setFormValidated] = React.useState(false);
+
+	const [formData, setFormData] = React.useState({
+		emailID: "",
+		password: "",
+	});
+	const handleLoginSubmit = async (e) => {
+		e.preventDefault();
+
+		if (e.currentTarget.checkValidity()) {
+			const { data } = await axios.post(
+				"https://logathon-signin.herokuapp.com/web/login",
+				{
+					...formData,
+				}
+			);
+			if (data.status === "success") {
+				navigate("/posts");
+			}
+		} else {
+			setFormValidated(true);
+		}
+	};
+	return (
+		<div className="row" style={{ height: "98vh", overflowX: "hidden" }}>
+			<div className="offset-3 col-6 align-self-center">
+				<Card>
+					<Card.Body>
+						<h1 className="h3 text-center">Login</h1>
+						<Form
+							onSubmit={handleLoginSubmit}
+							noValidate
+							validated={formValidated}
+						>
+							<Form.Group controlId="email">
+								<Form.Label>Email</Form.Label>
+								<Form.Control
+									type="email"
+									placeholder="Email"
+									id="email"
+									value={formData?.emailID}
+									onChange={(e) =>
+										setFormData({ ...formData, emailID: e.target.value })
+									}
+									required
+								/>
+								<Form.Control.Feedback type="invalid">
+									Please Enter Email
+								</Form.Control.Feedback>
+							</Form.Group>
+							<Form.Group controlId="password">
+								<Form.Label>Password</Form.Label>
+								<Form.Control
+									type="password"
+									placeholder="Password"
+									id="password"
+									value={formData?.password}
+									onChange={(e) =>
+										setFormData({ ...formData, password: e.target.value })
+									}
+									required
+								/>
+								<Form.Control.Feedback type="invalid">
+									Please Enter Password
+								</Form.Control.Feedback>
+							</Form.Group>
+
+							<div className="text-center mt-2">
+								<Button type="submit">Login</Button>
+							</div>
+						</Form>
+						<div className="row mt-3">
+							<div className="col text-center">
+								Don't have an account ? <Link to="/">Sign Up</Link>
+							</div>
+						</div>
+					</Card.Body>
+				</Card>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
